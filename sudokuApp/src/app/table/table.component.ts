d82b1b357f2bfbc0517bf503725f0fcb7b28f0bb
monkata <monkataaa@abv.bg>
2019-06-14 18:44:29 +0300

    initial upload from sofia PC

---
+++ sudokuApp/src/app/table/table.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css']
+})
+export class TableComponent implements OnInit {
+
+  public tableArr: Object = {}
+
+  ngOnInit() {
+    this.fillUpArray()
+    console.log('table is', this.tableArr);
+    
+  }
+
+  public elCoutner = 1
+  // public currentNumber = 1
+ 
+  fillUpArray(){
+    for (let objCube = 0; objCube < 9; objCube++) {
+      let arr =  []
+      for (let row = 0; row < 3; row++) {
+          
+        for (let el = 0; el < 3; el++) {
+          arr.push(this.getName(objCube) +  this.elCoutner)
+          this.elCoutner++
+        }
+        this.tableArr[this.getName(objCube)] = arr
+        if (row == 2) {
+          this.elCoutner = 1
+        }
+      }
+      
+   
+    }
+
+  }
+
+  getName(index){
+    switch (index) {
+      case 0: return 'a';
+      case 1: return 'b';
+      case 2: return 'c';
+      case 3: return 'd';
+      case 4: return 'e';
+      case 5: return 'f';
+      case 6: return 'g';
+      case 7: return 'h';
+      case 8: return 'i';
+    }
+  }
+
+
+}
