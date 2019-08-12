d44c3459d3241438fae5cb1b345e46ea76baa1fa
monkata <monkataaa@abv.bg>
2019-08-12 13:12:33 +0300

    correction in server path

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/sudokuApp'));
+app.use(express.static('./dist/sudoku'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/sudokuApp/index.html'));
+res.sendFile(path.join(__dirname,'/dist/sudoku/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, function(){
+    console.log(`Express server listening on port ${this.address().port} in ${app.settings.env} mode`);
+})
