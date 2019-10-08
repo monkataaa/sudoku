2f077ea5732db8f350c862d3023df22fcfe2cf5a
monkata <monkataaa@abv.bg>
2019-10-08 18:49:32 +0300

    saved data in file. reading from files

--- e2e/tests/first.ts
+++ e2e/tests/first.ts
@@ -8,11 +8,11 @@ fixture`Getting Started`
 
 test('My first test', async t => {
 
-    // const articleHeader = await proba
+    const articleHeader = await proba
 
-    // // Obtain the text of the article header
-    // let headerText = await articleHeader.innerText;
-    // console.log('headerText', headerText);
+    // Obtain the text of the article header
+    let headerText = await articleHeader.innerText;
+    console.log('headerText', headerText);
 
 
     let resultArr = []
@@ -21,32 +21,40 @@ test('My first test', async t => {
     const columnCount = await table.find('tr').nth(0).find('td').count;
     console.log('rowCount', rowCount);
 
-    // for (let i = 2; i < rowCount; i++) {
-    //     let obj = {}
-    //     let district = await table.find('tr').nth(i).find('td').nth(0).textContent;
-    //     let oneRoom = await table.find('tr').nth(i).find('td').nth(2).textContent;
-    //     let twoRooms = await table.find('tr').nth(i).find('td').nth(5).textContent;
-    //     let three = await table.find('tr').nth(i).find('td').nth(8).textContent;
-    //     obj['district'] = district;
-    //     obj["oneRoom"] = Number(oneRoom.trim().replace(' ', ''));
-    //     obj["twoRooms"] = Number(twoRooms.trim().replace(' ', ''));
-    //     obj["three"] = Number(three.trim().replace(' ', ''));
-    //     resultArr.push(obj)
-    //     if (district == 'м-т Киноцентъра') {
-    //         console.log(' posledniq e ', i);
-    //         break;
-    //     }
-    //     // console.log('kvartal', kvartal);
-    //     // console.log('ednostajni', ednostajni);
-    //     // console.log('dvustajni', dvustajni);
-    //     // console.log('tristajni', tristajni);
+    for (let i = 2; i < rowCount; i++) {
+        let obj = {}
+        let district = await table.find('tr').nth(i).find('td').nth(0).textContent;
+        let oneRoom = await table.find('tr').nth(i).find('td').nth(2).textContent;
+        let twoRooms = await table.find('tr').nth(i).find('td').nth(5).textContent;
+        let three = await table.find('tr').nth(i).find('td').nth(8).textContent;
+        obj['district'] = district;
+        obj["oneRoom"] = Number(oneRoom.trim().replace(' ', ''));
+        obj["twoRooms"] = Number(twoRooms.trim().replace(' ', ''));
+        obj["three"] = Number(three.trim().replace(' ', ''));
+        resultArr.push(obj)
+        if (district == 'м-т Киноцентъра') {
+            console.log(' posledniq e ', i);
+            break;
+        }
+        // console.log('kvartal', kvartal);
+        // console.log('ednostajni', ednostajni);
+        // console.log('dvustajni', dvustajni);
+        // console.log('tristajni', tristajni);
 
-    // }
+    }
 
-    let blob = await new Blob(['Hello world!'], {
-        type: "{ type: 'text/plain;charset=utf-8' }"
+    
+
+    const fs = require('fs');
+
+    fs.writeFile("../../src/assets/resultsData.json", JSON.stringify(resultArr), function (err) {
+
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("The file was saved!");
     });
-    FileSaver.saveAs(blob, 'newcontrol.txt');
 
     // console.log(resultArr);
 
--- src/app/prices/prices.component.ts
+++ src/app/prices/prices.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import * as FileSaver from 'file-saver';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 declare var require: any
 
@@ -10,14 +10,25 @@ declare var require: any
 })
 export class PricesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private httpService: HttpClient) { }
 
+  
   ngOnInit() {
-
+    this.httpService.get('./assets/resultsData.json').subscribe(
+      data => {
+        this.pricesArr = data as string [];	 // FILL THE ARRAY WITH DATA.
+      },
+      (err: HttpErrorResponse) => {
+        console.log (err.message);
+      }
+    );
   }
-
+  
+  pricesArr = []
   lastOrderType = 'asc';
   selectedOrder = '';
+
+
 
   limits = {
     'oneRoom' : 50000,
@@ -38,83 +49,6 @@ export class PricesComponent implements OnInit {
     }
     this.pricesArr = this.pricesArr.sort((a, b) => (a[selector] > b[selector]) ? asc : desc)
   }
-
-  saveIt() {
-    // let blob = new Blob(['Hello world!'], {
-    //   type:  "{ type: 'text/plain;charset=utf-8' }"
-    // });
-    // FileSaver.saveAs(blob, 'newcontrol.txt');
-  //   const fs = require('browserify-fs');
-
-  //   fs.writeFileSync("myFile", "Hey there!", function(err) {
-  //     if(err) {
-  //         return console.log(err);
-  //     }
-  
-  //     console.log("The file was saved!");
-  // }); 
-
-
-  // console.log("Going to create directory /tmp/test");
-  // console.log('fs is ', fs);
-  // fs.mkdir('/tmp/test',function(err){
-  //    if (err) {
-  //        return console.error(err);
-  //    }
-  //    console.log("Directory created successfully!");
-  // });
-  }
-
-
-  pricesArr = [
-    {
-      district: 'Банишора',
-      oneRoom: 44222,
-      twoRooms: 64151,
-      three: 91200
-    },
-    {
-      district: 'Белите брези',
-      oneRoom: 53190,
-      twoRooms: 74100,
-      three: 118512
-    },
-    {
-      district: 'Бенковски',
-      oneRoom: 37045,
-      twoRooms: 42655,
-      three: 71250
-    },
-    {
-      district: 'Борово',
-      oneRoom: 48070,
-      twoRooms: 70585,
-      three: 118750
-    },
-    {
-      district: 'Ботунец',
-      oneRoom: 18989,
-      twoRooms: 21850,
-      three: 34200
-    },
-    {
-      district: 'Бояна',
-      oneRoom: 58125,
-      twoRooms: 81975,
-      three: 141550
-    },
-    {
-      district: 'Бъкстон',
-      oneRoom: 48996,
-      twoRooms: 71383,
-      three: 104499
-    },
-    {
-      district: 'Витоша',
-      oneRoom: 42028,
-      twoRooms: 68896,
-      three: 110825
-    }]
 
 
 }
